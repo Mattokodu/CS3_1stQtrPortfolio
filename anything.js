988deb7efbdb7ed17074b5943498ee7bcd9a0cac
Mattokodu <b2029misespocia@pshs.edu.ph>
2025-08-29 16:01:18 +0800

    yes

--- anything.js
+++ anything.js
@@ -1,11 +1,14 @@
 console.log("AGFDGSDGFg");
 
 rl = require('readline') // #include<bits/stdc++.h> sa c++
-rli = rl.createInterface({
+rli = rl.createInterface({ // adding the input
     input: process.stdin,
 })
 
-rli.on("line", get_station)
+
+/*
+
+rli.on('line', get_station) IT CONTINUES ON CHECKING DO I SEE A 'LINE' AHGTHAHAGHAHAHAHAH
 
 function get_station(num) {
 
@@ -16,4 +19,33 @@ function get_station(num) {
         default:
             console.log('TEST');
     }
+}
+
+*/
+
+rli.on('line', isPrime)
+
+function isPrime(num) {
+
+    if (num < 2) {
+        console.log("Number is not prime nor composite.");
+    }
+    else if (num == 2) {
+        console.log("Number is prime.");
+    }
+    else {
+        let isPrime = true;
+        for (let i = 2; i < num; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+            }
+        }
+        if (isPrime) {
+            console.log("Number is prime.");
+        }
+        else {
+            console.log("Number is not prime.");
+        }
+    }
+
 }
